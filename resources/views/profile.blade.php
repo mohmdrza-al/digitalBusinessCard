70716b88c450dc5ffe56a7c5b941278d726b7c31
mohmdrza <m.alaghebandshz@gmail.com>
2023-01-13 04:52:24 -0800

    DBC V1

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>M.Alagheband's profile</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Hind"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <link rel="stylesheet" href="https://project-1.storage.iran.liara.space/profile/profileStyle.css">
+    <link rel="stylesheet" href="https://project-1.storage.iran.liara.space/profile/myOwnStyle.css">
+
+</head>
+<body>
+<aside class="profile-card">
+    <header>
+        <!-- here’s the avatar -->
+        <a href="#">
+            <img src="https://project-1.storage.iran.liara.space/20220916_113958.jpg"/>
+        </a>
+
+        <!-- the username -->
+        <h2>Mohammadreza Alagheband</h2>
+
+        <!-- and role or location -->
+        <h3>Back-end Developer</h3>
+
+    </header>
+
+    <!-- bio -->
+    <div class="profile-bio">
+
+        <p>Backend programmer proficient in Laravel and NodeJS...
+            <br> And eager to learn new things in the world of technology
+        </p>
+
+    </div>
+
+    <!-- some social links to contact -->
+    <ul class="profile-social-links">
+
+        <!-- call -->
+        <li>
+            <div class="tooltip">
+                <span class="tooltiptext">Call</span>
+                <a href="tel:+989308783056">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path
+                            d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3.445 17.827c-3.684 1.684-9.401-9.43-5.8-11.308l1.053-.519 1.746 3.409-1.042.513c-1.095.587 1.185 5.04 2.305 4.497l1.032-.505 1.76 3.397-1.054.516z"/>
+                    </svg>
+                </a>
+            </div>
+        </li>
+
+        <!-- email -->
+        <li>
+            <div class="tooltip">
+                <span class="tooltiptext">Email</span>
+                <a href="mailto:m.alaghebandshz@gmail.com">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path
+                            d="M12 .02c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.99 6.98l-6.99 5.666-6.991-5.666h13.981zm.01 10h-14v-8.505l7 5.673 7-5.672v8.504z"/>
+                    </svg>
+                </a>
+            </div>
+        </li>
+        <!-- cv -->
+        <li>
+            <div class="tooltip">
+                <span class="tooltiptext">Download my cv</span>
+                <a href="https://project-1.storage.iran.liara.space/m-alagheband%27s%20cv.pdf">
+                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd"
+                         clip-rule="evenodd">
+                        <path
+                            d="M3 24h19v-23h-1v22h-18v1zm17-24h-18v22h18v-22zm-3 17h-12v1h12v-1zm0-3h-12v1h12v-1zm0-3h-12v1h12v-1zm-7.348-3.863l.948.3c-.145.529-.387.922-.725 1.178-.338.257-.767.385-1.287.385-.643 0-1.171-.22-1.585-.659-.414-.439-.621-1.04-.621-1.802 0-.806.208-1.432.624-1.878.416-.446.963-.669 1.642-.669.592 0 1.073.175 1.443.525.221.207.386.505.496.892l-.968.231c-.057-.251-.177-.449-.358-.594-.182-.146-.403-.218-.663-.218-.359 0-.65.129-.874.386-.223.258-.335.675-.335 1.252 0 .613.11 1.049.331 1.308.22.26.506.39.858.39.26 0 .484-.082.671-.248.187-.165.322-.425.403-.779zm3.023 1.78l-1.731-4.842h1.06l1.226 3.584 1.186-3.584h1.037l-1.734 4.842h-1.044z"/>
+                    </svg>
+                </a>
+            </div>
+        </li>
+
+        <!-- github -->
+        <li>
+            <div class="tooltip">
+                <span class="tooltiptext">Github</span>
+                <a href="https://github.com/mohmdrza-al">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path
+                            d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                    </svg>
+                </a>
+            </div>
+        </li>
+    </ul>
+
+</aside>
+<script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+</body>
+</html>
