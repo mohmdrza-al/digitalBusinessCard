63bd768b8f2c079039971ae2e61b8032626b21ee
mohmdrza <m.alaghebandshz@gmail.com>
2023-01-14 12:57:05 -0800

    Authentication has been created and web.php updated.

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/profile', function () {
     return view('profile');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
